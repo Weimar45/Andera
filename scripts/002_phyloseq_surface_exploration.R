c10453413fb9f036fd123ee8680ddb9f8eddbf4b
Alex42NGNAR <alejandronavasgonzalez6@gmail.com>
2023-06-09 00:06:50 +0200

    update exploration

--- scripts/002_phyloseq_surface_exploration.R
+++ scripts/002_phyloseq_surface_exploration.R
@@ -11,8 +11,17 @@ GlobalPatterns
 # Identificador de las muestras.
 sample_names(GlobalPatterns) 
 
+# Número de muestras en el objeto phyloseq. 
+nsamples(GlobalPatterns)
+
 # Variables que definen la taxonomía.
 rank_names(GlobalPatterns) 
 
 # Variables de los metadatos (se suelen definir en un excel).
 sample_variables(GlobalPatterns) 
+
+# Número de taxones en el objeto phyloseq.
+ntaxa(GlobalPatterns)
+
+# Ver la tabla de Unidades Taxonómicas Operativas (OTUs)
+otu_table(GlobalPatterns)[1:5, 1:5]
